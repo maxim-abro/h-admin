3fe8ee2f9c11118c4ba161288446c51574e7430b
Maxim Abrosimov <maxim.abro@yandex.ru>
2023-11-22 23:16:28 +0300

    todo: users_stat

---
+++ src/views/users_stat/index.vue
@@ -0,0 +1,9 @@
+<template>
+<div class="relative h-full flex flex-auto flex-col px-4 sm:px-6 py-4 sm:py-6 md:px-8">
+  <h1 class="mb-4 text-2xl font-bold">Активность администратора</h1>
+</div>
+</template>
+
+<script setup lang="ts">
+
+</script>
